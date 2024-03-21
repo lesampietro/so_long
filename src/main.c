1a214e1a843a0257ed69a8666c4aaef5a957bf7b
Letícia Sampietro <lsampiet@student.42sp.org.br>
2024-03-21 18:25:14 -0300

    starting the project

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include "../MLX42/include/MLX42/MLX42.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#define WIDTH 256
+#define HEIGHT 256
+
+int	main()
+{
+	printf("Alo alo");
+}
