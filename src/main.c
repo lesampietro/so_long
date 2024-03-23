535b62d4bf016f5d70e45ed2ebe9a7011865b613
Letícia Sampietro <lsampiet@student.42sp.org.br>
2024-03-23 17:37:25 -0300

    compilation test ok

--- src/main.c
+++ src/main.c
@@ -1,11 +1,42 @@
-#include "../MLX42/include/MLX42/MLX42.h"
+#include "../lib/MLX42/include/MLX42/MLX42.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
-#define WIDTH 256
-#define HEIGHT 256
+#define WIDTH 640
+#define HEIGHT 360
 
-int	main()
+static void error(void)
 {
-	printf("Alo alo");
+	puts(mlx_strerror(mlx_errno));
+	exit(EXIT_FAILURE);
+}
+
+int32_t main(void)
+{
+	// Start mlx
+	mlx_t *mlx = mlx_init(WIDTH, HEIGHT, "Capybara Game", false);
+	if (!mlx)
+		error();
+
+	// Try to load the file
+	mlx_texture_t *texture = mlx_load_png("./images/menu_bg.png");
+	if (!texture)
+		error();
+
+	// Convert texture to a displayable image
+	mlx_image_t *img = mlx_texture_to_image(mlx, texture);
+	if (!img)
+		error();
+
+	// Display the image
+	if (mlx_image_to_window(mlx, img, 0, 0) < 0)
+		error();
+
+	mlx_loop(mlx);
+
+	mlx_delete_image(mlx, img);
+	mlx_delete_texture(texture);
+
+	// Optional, terminate will clean up any leftover images (not textures!)
+	mlx_terminate(mlx);
+	return (EXIT_SUCCESS);
 }
