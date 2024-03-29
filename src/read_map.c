0a8152765d006fb7db8b2cbe036833274284ad34
Letícia Sampietro <lsampiet@student.42sp.org.br>
2024-03-29 14:51:56 -0300

    gnl and printf functioning

--- src/read_map.c
+++ src/read_map.c
@@ -6,7 +6,7 @@
 /*   By: lsampiet <lsampiet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 18:27:29 by lsampiet          #+#    #+#             */
-/*   Updated: 2024/03/27 23:05:18 by lsampiet         ###   ########.fr       */
+/*   Updated: 2024/03/29 14:50:52 by lsampiet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,12 @@ char	**read_map(char *map)
 	if(!map)
 		exit(ft_printf("Map is empty"));
 	fd = open(map, O_RDONLY);
-	new_map = get_next_line(fd);
-	while(new_map != '\0')
-		ft_printf(new_map);
+	while((new_map = get_next_line(fd)))
+	{
+		ft_printf("%s", new_map);
+		free(new_map);
+	}
+	// ft_printf("line %d->NULL", line_count++);
 	close(fd);
 	return (NULL);
 }
--- src/so_long.c
+++ src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: lsampiet <lsampiet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 17:39:51 by lsampiet          #+#    #+#             */
-/*   Updated: 2024/03/27 22:41:19 by lsampiet         ###   ########.fr       */
+/*   Updated: 2024/03/29 14:50:12 by lsampiet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,15 @@ int32_t main(int argc, char **argv)
 {
 	char	**new_map;
 	
-	if(argc == 2)
-	{
-		new_map = read_map(argv[1]);
-		if(new_map)
-		{
-			return (1);
-			// check_map_ext(argv[1]);
-			// check_map_shape(argv[1]);
-			// check_map_chars(argv[1]);
-		}
-		exit(ft_printf("Could not open map"));
-	}
-	exit(ft_printf("Missing argument"));
+	if(argc != 2)
+		exit(ft_printf("Missing argument"));
+	new_map = read_map(argv[1]);
+	// exit(ft_printf("Could not open map"));
 }
+		// if(new_map)
+		// {
+		// 	return (1);
+		// 	// check_map_ext(argv[1]);
+		// 	// check_map_shape(argv[1]);
+		// 	// check_map_chars(argv[1]);
+		// }
